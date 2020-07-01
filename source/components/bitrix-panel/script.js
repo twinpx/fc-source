dda28d03e0cf2910f34b08952f57193ca2eaffbd
Tatiana <djembuka@gmail.com>
2020-07-01 21:11:40 +0300

    Bitrix panel

---
+++ source/components/bitrix-panel/script.js
@@ -0,0 +1,14 @@
+( function($) {
+
+  'use strict';
+  
+  $( function() {
+    
+    $( '#bx-panel-hider' ).bind( 'click', function() {
+      var $headerPanel = $( '.b-header-panel' );
+      $header.data( 'topBorder', $header.offset().top );
+    });
+    
+  });
+
+}( jQuery ));
