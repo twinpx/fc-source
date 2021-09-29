30a2a4b458676a5c26f7d8ef452275302a827c21
djembuka <djembuka@gmail.com>
2021-09-29 12:23:00 +0300

    SMM button

--- source/components/articles.article/script.js
+++ source/components/articles.article/script.js
@@ -1,14 +1,28 @@
-﻿( function($) {
-
+﻿(function ($) {
   'use strict';
-  
-  $( function() {
-  
-    $( '.b-article' ).addtocopy({ htmlcopytxt: '<br>Подробнее: <a href="' + window.location.href + '">' + window.location.href + "</a>", minlen: 50, addcopyfirst: false });
-  
+
+  $(function () {
+    $('.b-article').addtocopy({
+      htmlcopytxt:
+        '<br>Подробнее: <a href="' +
+        window.location.href +
+        '">' +
+        window.location.href +
+        '</a>',
+      minlen: 50,
+      addcopyfirst: false,
+    });
+
+    document.querySelectorAll('[data-reslist]').forEach(function (elem) {
+      const reslistNumber = elem.getAttribute('data-reslist');
+      const placeholder = document.createElement('div');
+      placeholder.innerHTML = '<span>Placeholder</span>';
+      elem.after(placeholder);
+      elem.remove();
+    });
+
     /*if ( window.BX ) {
       BX.addCustomEvent( "onFrameDataReceived", function () {});
     }*/
   });
-
-}( jQuery ));
+})(jQuery);
---
+++ source/modules/smm-button/script.js
@@ -0,0 +1,26 @@
+﻿if (document.querySelector('.b-smm-ajax')) {
+  setTimeout(function () {
+    var $smmAjax = $('.b-smm-ajax');
+    $.ajax({
+      url: $smmAjax.data('ajax-url'),
+      type: $smmAjax.data('ajax-method'), //GET
+      dataType: 'html',
+      success: function (html) {
+        var $smm = $(html);
+        $smm.appendTo('body');
+        var $smmButton = $smm.find('.b-smm-button');
+        $smmAjax.remove();
+        setTimeout(function () {
+          $smmButton.addClass('i-show');
+        }, 0);
+      },
+      error: function (a, b, c) {
+        if (window.console) {
+          console.log(a);
+          console.log(b);
+          console.log(c);
+        }
+      },
+    });
+  }, 8000);
+}
